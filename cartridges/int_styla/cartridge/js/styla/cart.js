5699bfc969f70b5b3d31682dd0615210a4e924d9
ssachtleben <ssachtleben@gmail.com>
2018-03-08 10:00:59 +0100

    #1964 format code

--- cartridges/int_styla/cartridge/js/styla/cart.js
+++ cartridges/int_styla/cartridge/js/styla/cart.js
@@ -2,30 +2,34 @@
 
 var minicart = null,
 
-	/**
-	 * Expose add-to-cart functionality which can be used by Styla JS library.
-	 */
-	addToCart = function(productID, qty) {
-        return new Promise(function( resolve, reject ) {
-    		$.ajax({
-    			type: 'POST',
-    			url: Urls.addProduct + '?format=ajax',
-    			data: {
-    				pid : productID,
-    				Quantity : qty
-    			}
-    		}).done(function(response) {
-    			minicart.show(response);
+    /**
+     * Expose add-to-cart functionality which can be used by Styla JS library.
+     */
+    addToCart = function(productID, qty) {
+        return new Promise(function(resolve, reject) {
+            $.ajax({
+                type: 'POST',
+                url: Urls.addProduct + '?format=ajax',
+                data: {
+                    pid: productID,
+                    Quantity: qty
+                }
+            }).done(function(response) {
+                minicart.show(response);
                 resolve(response);
-    		}).fail(function(jqXHR, textStatus, errorThrown) {
+            }).fail(function(jqXHR, textStatus, errorThrown) {
                 reject(errorThrown || 'error');
             });
         });
-	};
+    };
 
 module.exports = function(minicartObject) {
-	window.styla = window.styla || {callbacks: []};
-	styla && styla.callbacks && styla.callbacks.push({'addToCart': addToCart});
+    window.styla = window.styla || {
+        callbacks: []
+    };
+    styla && styla.callbacks && styla.callbacks.push({
+        'addToCart': addToCart
+    });
 
-	minicart = minicartObject;
+    minicart = minicartObject;
 };
