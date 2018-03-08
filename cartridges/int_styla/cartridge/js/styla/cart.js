2f8e82dafecfbfd8845b7b9aff253dc0e0e4619c
ssachtleben <ssachtleben@gmail.com>
2018-03-08 09:58:14 +0100

    #1694 added promise to addtocart function response

--- cartridges/int_styla/cartridge/js/styla/cart.js
+++ cartridges/int_styla/cartridge/js/styla/cart.js
@@ -3,24 +3,29 @@
 var minicart = null,
 
 	/**
-	 * Expose add-to-cart functionality which can be used by Styla JS library. 
+	 * Expose add-to-cart functionality which can be used by Styla JS library.
 	 */
 	addToCart = function(productID, qty) {
-		$.ajax({
-			type: 'POST',
-			url: Urls.addProduct + '?format=ajax',
-			data: {
-				pid : productID,
-				Quantity : qty
-			}
-		}).done(function(response) {
-			minicart.show(response);
-		});
+        return new Promise(function( resolve, reject ) {
+    		$.ajax({
+    			type: 'POST',
+    			url: Urls.addProduct + '?format=ajax',
+    			data: {
+    				pid : productID,
+    				Quantity : qty
+    			}
+    		}).done(function(response) {
+    			minicart.show(response);
+                resolve(response);
+    		}).fail(function(jqXHR, textStatus, errorThrown) {
+                reject(errorThrown || 'error');
+            });
+        });
 	};
 
 module.exports = function(minicartObject) {
 	window.styla = window.styla || {callbacks: []};
 	styla && styla.callbacks && styla.callbacks.push({'addToCart': addToCart});
-	
+
 	minicart = minicartObject;
 };
